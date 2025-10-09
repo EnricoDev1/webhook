c225a0e1cf28c95f15c27272d8a763a75ac6ad72
noahterenzianii <noahterenziani@gmail.com>
2025-10-09 09:38:22 +0200

    fixed init.sh

--- init.sh
+++ init.sh
@@ -2,13 +2,10 @@
 export JWT_SECRET=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 64)
 echo "JWT_SECRET generato: $JWT_SECRET"
 
-if [ "$1" = "-build" ]; then
-    echo "Eseguo docker compose up con --build..."
+if [ "$1" = "--build" ]; then
     docker compose up --build
 elif [ "$1" = "--down" ]; then
-    echo "Eseguo docker compose up in detached mode..."
     docker compose down
 else
-    echo "Eseguo docker compose up..."
     docker compose up
 fi
