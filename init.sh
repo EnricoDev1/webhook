a693ad2c5e692ada4864b2116371e349be727709
noahterenzianii <noahterenziani@gmail.com>
2026-01-07 17:30:39 +0100

    updated init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ if [ "$1" = "--build" ]; then
     docker compose up --build
 elif [ "$1" = "--down" ]; then
     docker compose down
-elif [ "$1" = "-d" ]; then 
-    docker compose up -d
+elif [ "$1" = "--remove" ]; then 
+    docker compose down -v
 else
     docker compose up
 fi
