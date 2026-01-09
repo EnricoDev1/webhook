03b2775ec0d55745c41e11093f58b70ef353c0e1
EnricoDev <enrioffi@gmail.com>
2026-01-09 15:23:00 +0100

    changed nginx conf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/v/',  
+  base: '/v/',
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ['hook.enricodev.xyz']
   },
   plugins: [react()],
 });
