734fdab490f5fdf7b9203396beb1ec6d814d2f98
EnricoDev <enrioffi@gmail.com>
2026-02-17 17:30:32 +0100

    removed production docker compose configs

--- init.sh
+++ init.sh
@@ -5,10 +5,6 @@ elif [ "$1" = "--down" ]; then
     docker compose down
 elif [ "$1" = "--remove" ]; then 
     docker compose down -v
-elif [ "$1" = "--prod" ]; then
-    sed -i 's|COPY default.conf|COPY default.prod.conf|' "./nginx/Dockerfile"
-    echo "[+] nginx Dockerfile updated"
-    docker compose -f docker-compose.yml -f docker-compose.prod.yml up --build $2
 else
     docker compose up
 fi
